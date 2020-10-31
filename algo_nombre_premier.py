71536446f4783e79c5e4b0f5401c657e19fbfc0a
Marc-Antoine Marier <marc-antoine.marier.1@ulaval.ca>
2020-10-31 02:25:43 -0400

    list_prime_number

--- algo_nombre_premier.py
+++ algo_nombre_premier.py
@@ -1,5 +1,21 @@
 #tester i nombres
-#n = 10
-#for i in range(n):
-#   for x in range(i):
-#        if i//x:
+def prim(nb):
+    for x in range(2, nb-1): #tester si le nb est premeier
+        if nb % x == 0:
+            return print(f"Le nombre {nb} n'est pas premier.")
+    return print(f"Le nombre {nb} est premier.")
+
+def vprime(n):
+    for x in range(2, n-1): #tester si le nb est premeier
+        if n % x == 0:
+            return False
+    return True
+
+def liste_prime(z):
+    liste = []
+    for i in range(2, z):
+        if vprime(i):
+            liste.append(i)
+    return print(liste)
+
+liste_prime(100000)
