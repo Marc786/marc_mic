c48aa9999b24144f1ebb924b6b14f791a0bc93ea
Marc-Antoine Marier <marc-antoine.marier.1@ulaval.ca>
2020-12-08 05:15:26 -0500

    algo bot

--- projects/Auto_up_down.py
+++ projects/Auto_up_down.py
@@ -21,8 +21,10 @@ def guess():
 def guess_auto_random():
     tranche = int(input("Sur quel tranche de nombre voulez vous jouer (nombre positif) ?"))
     if tranche < 0:
-        raise ValueError
+        raise ValueError("La tranche doit etre superieur a 0")
     nombre = int(input("Quel est le nombre que le bot doit trouver ?"))
+    if nombre > tranche:
+        raise ValueError("Le nombre doit etre plus petit ou egal a la tranche")
     check = True
     coups = 0
     while check:
@@ -35,10 +37,28 @@ def guess_auto_random():
 def guess_auto_algo():
     tranche = int(input("Sur quel tranche de nombre voulez vous jouer (nombre positif) ?"))
     if tranche < 0:
-        raise ValueError
+        raise ValueError("La tranche doit etre superieur a 0")
     nombre = int(input("Quel est le nombre que le bot doit trouver ?"))
+    if nombre > tranche:
+        raise ValueError("Le nombre doit etre plus petit ou egal a la tranche")
     check = True
     coups = 0
+    low_guess = 0
+    high_guess = tranche
+    guess = random.randint(0, tranche)
+    #guess = tranche/2
     while check:
         coups += 1
-        
+        if guess == nombre:
+            check = False
+        elif guess > nombre:
+            high_guess = guess
+            guess = guess - (guess-low_guess)//2
+            print(guess)
+        elif guess < nombre:
+            low_guess = guess
+            guess = guess + (high_guess-guess)//2
+            print(guess)
+    print(f'Le bot a trouve en {coups} coups.')
+
+guess_auto_algo()
