97532796afaf061c3760868765c69cab8987dce5
Marc-Antoine Marier <marc-antoine.marier.1@ulaval.ca>
2020-12-28 23:10:11 -0500

    learning mic lol

--- projects/Auto_up_down.py
+++ projects/Auto_up_down.py
@@ -3,20 +3,52 @@ import random
 
 
 def guess():
+    parties = 0
+    replay = True
+    points = 0
     tranche = int(input("Sur quel tranche de nombre voulez vous jouer (nombre positif) ?"))
     if tranche < 0:
         raise ValueError
-    rnb = random.randint(0,tranche)
-    check = True
-    while check:
-        guess = int(input("Choisissez un nombre au hazard :"))
-        if guess == rnb:
-            check = False
-            print("gg")
-        elif guess > rnb:
-            print("Trop Haut!")
-        elif guess < rnb:
-            print("Trop Bas!")
+    while replay:
+        parties += 1
+        rnb = random.randint(0, tranche)
+        check = True
+        coups = 0
+        while check:
+            coups += 1
+            guess = int(input(f"Choisissez un nombre au hazard entre 0 et {tranche} :"))
+            if guess == rnb:
+                check = False
+                print("gg")
+            elif guess > rnb:
+                print("Trop Haut!")
+            elif guess < rnb:
+                print("Trop Bas!")
+        print(f'Vous avez trouve en {coups} coups. ') # ==, <=, >=, <, >, !=, and, or
+        if coups == 1:
+            print(f'Vous avez obtenu 10 points.')
+            points += 10
+        elif coups == 2:
+            print(f'Vous avez obtenu 7 points.')
+            points += 7
+        elif coups == 3 or coups == 4:
+            print(f'Vous avez obtenu 5 points.')
+            points += 5
+        elif coups == 5 or coups == 6:
+            print(f'Vous avez obtenu 3 points.')
+            points += 3
+        elif coups > 6:
+            print(f'Vous avez rien obtenu (Vous etes nul)')
+        
+        question2 = input('Voulez-vous un recapitulatif des points? ([y] = oui, [n] = non)')
+        if question2 == 'y':
+            print(f'{points} points')
+        question = input('Voulez-vous rejouer? ([y] = oui, [n] = non)')
+        if question == 'n':
+            replay = False
+        
+    print(f'Vous avez obtenu {points} points en {parties} parties')
+
 
 def guess_auto_random():
     tranche = int(input("Sur quel tranche de nombre voulez vous jouer (nombre positif) ?"))
@@ -46,7 +78,6 @@ def guess_auto_algo():
             error = False
         except ValueError as identifier:
             print(identifier)
-            error = True
     error = True
     while error:
         try:
@@ -56,7 +87,6 @@ def guess_auto_algo():
             error = False
         except ValueError as identifier:
             print(identifier)
-            error = True
     check = True
     coups = 0
     low_guess = 0
--- projects/algo_nombre_premier.py
+++ projects/algo_nombre_premier.py
@@ -13,9 +13,27 @@ def liste_prime(z):
         if vprime(i):
             liste.append(i)
     return print(liste)
+    
+def liste2(z):
+    liste = []
+    for i in range(2, z):
+        verif = True
+        for x in range(2, i-1):
+            if i % x == 0:
+                verif = False
+                break
+        if verif:
+            liste.append(i)
+    return liste
 
 r = 100
-debut = time.perf_counter()
-liste_prime(r)
-fin = time.perf_counter()
-print(f"La liste des nombres premiers parmis les {r} premiers nombres a été trouvé en {fin - debut} secondes.")
+# prime = ''
+# liste = liste2(r)
+# for i in liste:
+#     prime += (str(i) + ' ')
+# debut = time.perf_counter()
+# liste_prime(r)
+# liste2(r)
+# fin = time.perf_counter()
+# print(f"La liste des nombres premiers parmis les {r} premiers nombres a été trouvé en {fin - debut} secondes.")
+# print(f"les nombres premiers de 0 a {r} sont : {prime}")
